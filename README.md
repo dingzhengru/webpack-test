66c46d59fe424f40ec33e4b25f96fb7e190b994c
dingzhengru <qws7869vdx@gmail.com>
2019-11-04 20:05:08 +0800

    update get started

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# webpack-test
-My webpack test
+# My webpack test
+
+|- package.json
+|- webpack.config.js
+|- index.html
+|- /dist
+  |- main.js
+  |- index.html
+|- /src
+  |- index.js
+|- /node_modules
+
+## bundle src to dist
+```npx webpack```  
+or(需要在package.json => "scripts" add "build": "webpack")  
+```npm run build```
+
+## index.html
+### Import your Js & Library
+```
+<script src="https://unpkg.com/lodash@4.16.6"></script>
+<script src="./src/index.js"></script>
+```
+## src/index.js
+### Import Library
+```
+import _ from 'lodash'
+```
+
+## package.json
+## webpack.config.js
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+
+
+// mode: "production" | "development" | "none"
+// entry: 入口js檔
+// output: 出口js擋  filename, publicPath, library, libraryTarget
+/* module: 關於模板配置 rules
+    rules:[
+        {
+            test: /\.jsx?$/,
+            include: [
+                path.resolve(__dirname, "app")
+            ],
+            exclude:[
+                path.resolve(__dirname, "app/demo-files")
+            ]
+            // 以上三個都是篩選用的條件
+            // test是用正則表達式
+            // include,exclude是用絕對路徑
+            // exclude必是不匹配的選項(建議用include，不要用此)
+            
+            issuer: { test, include, exclude }
+            // 導入條件
+            
+            loader: "babel-loader"
+            options: {
+                presets: ["es2015"]
+            }
+            // loader的選項
+        }
+    ]
+*/
+// resolve: {} 解析modules請求的選項
+// performance: { hints: "warning" | "error" | false } 設定效能提示
+// devtool: "source-map" 設定browser devtools的顯示訊息(source-map最詳細)
+// context: __dirname webpack的主目錄
+// target: "web" 設定運行環境
+// externals
+// serve: { port: 1337, content: './dist' //... } 為webpack-serve提供選項
+// stats: "errors-only" 設定要顯示的bundle(打包)訊息
+// devServer
+// plugins
+const path = require('path');
+
+module.exports = {
+    mode: "none",
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+};
